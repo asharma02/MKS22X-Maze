3d21f0e312f7850e9286cd73af0c41dc4fcdf703
Aryan Sharma <asharma00@stuy.edu>
2019-03-04 00:54:15 -0500

    works, tested TY MICHAEL FOR HELPING ME DEBUG

--- Maze.java
+++ Maze.java
@@ -11,15 +11,12 @@ public class Maze{
     private boolean animate;//false by default
 
     /*Constructor loads a maze text file, and sets animate to false by default.
-
       1. The file contains a rectangular ascii maze, made with the following 4 characters:
       '#' - Walls - locations that cannot be moved onto
       ' ' - Empty Space - locations that can be moved onto
       'E' - the location of the goal (exactly 1 per file)
       'S' - the location of the start(exactly 1 per file)
-
       2. The maze has a border of '#' around the edges. So you don't have to check for out of bounds!
-
       3. When the file is not found OR the file is invalid (not exactly 1 E and 1 S) then:
          throw a FileNotFoundException or IllegalStateException
     */
@@ -101,12 +98,9 @@ public class Maze{
 
     /*
       Recursive Solve function:
-
       A solved maze has a path marked with '@' from S to E.
-
       Returns the number of @ symbols from S to E when the maze is solved,
       Returns -1 when the maze has no solution.
-
       Postcondition:
         The S is replaced with '@' but the 'E' is not.
         All visited spots that were not part of the solution are changed to '.'
@@ -141,5 +135,4 @@ public class Maze{
 
             return -1; //so it compiles
     }
-
 }
